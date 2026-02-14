2542ef6fc5bf40cf5dfd6edd7525fe62bf3e01ff
Outer Cloud <outercloudstudio@gmail.com>
2026-02-14 05:05:00 -0800

    code gen

---
+++ codegen.py
@@ -0,0 +1,59 @@
+import json
+
+# Read the JSON file
+with open('balancing_weights.json', 'r') as f:
+    model_params = json.load(f)
+
+input_names = ["a", "b", "c", "d"]
+
+for i in range(4):
+    line =  f"l_0_{i} = 0"
+
+    for j in range(4):
+        weight = model_params['fc1']['weights'][i][j]
+
+        operation = "+"
+
+        if weight < 0:
+            operation = "-"
+            weight = -weight
+
+        line += " " + operation + " " + input_names[j] + " * " + str(weight)
+
+    bias_operation = "+"
+
+    bias = model_params['fc1']['bias'][i]
+
+    if bias < 0:
+        bias_operation = "-"
+        bias = -bias
+
+    line += " " + bias_operation + " " + str(bias)
+
+    print(line)
+
+for i in range(2):
+    line =  f"l_1_{i} = 0"
+
+    for j in range(4):
+        weight = model_params['fc2']['weights'][i][j]
+
+        operation = "+"
+
+        if weight < 0:
+            operation = "-"
+            weight = -weight
+
+        line += " " + operation + " l_0_" + str(j) + " * " + str(weight)
+
+    bias_operation = "+"
+
+    bias = model_params['fc2']['bias'][i]
+
+    if bias < 0:
+        bias_operation = "-"
+        bias = -bias
+
+    line += " " + bias_operation + " " + str(bias)
+
+    print(line)
--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from heir.mlir import I64, Secret
 
 @compile()  # defaults to scheme="bgv", OpenFHE backend, and debug=False
 def func(a: Secret[I64], b: Secret[I64], c: Secret[I64], d: Secret[I64]):
-    l_0 = 96 * c 
-    l_0_a = l_0 * l_0 * l_0
+    l_0_0 = 0 + a * 21 + b * 9 - c * 37 - d * 9 - 4
+    l_0_1 = 0 - a * 10 + b * 0 - c * 60 + d * 1 + 4
+    l_0_2 = 0 - a * 16 - b * 1 + c * 22 + d * 2 + 6
+    l_0_3 = 0 - a * 27 - b * 12 - c * 15 + d * 11 + 5
+    l_1_0 = 0 + l_0_0 * 11 + l_0_1 * 10 - l_0_2 * 8 + l_0_3 * 3 + 21
+    l_1_1 = 0 - l_0_0 * 8 - l_0_1 * 6 - l_0_2 * 4 - l_0_3 * 8 + 20
 
-    return output
+    return l_1_0 - l_1_1
 
 func.setup()
 
